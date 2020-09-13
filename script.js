16d0dff0c5fb1a463308a35947984bff547b93cf
hmorton77 <harold.m.morton@gmail.com>
2020-09-13 12:08:58 -0400

    formatting

--- script.js
+++ script.js
@@ -15,13 +15,14 @@ function random (min, max) {
 }
 function getPassword() {
 // step 1: get characters req's
-// conditions (number of letters, special symbols, etc.)
+// conditions (number of characters, special symbols, etc.)
   var passLength = parseInt(prompt("How many characters are required for your password? (8-128 characters"));
   while (passLength < 8 || passLength > 128) {
     var passLength = parseInt(prompt("Re-Enter Length: you must request a password within the character limits! (8-128 characters)"))
   }
   var special = confirm("Does your password need special characters?");
   var numbers = confirm("Does your password need numbers 0-9?");
+  
 // step 2: put all the req'ed chars into 1 array. 
   var randArray = []
   var randArray = randArray.concat(lowerCaseCharacters)
@@ -29,6 +30,7 @@ function getPassword() {
     var randArray = randArray.concat(specialCharacters)
   if (numbers)
     var randArray = randArray.concat(numerals)
+
 // step 3: from the array, randomly select x characters, x being the amount of characters req'ed  
   randChar = ""
   for (var i = 0; i < passLength; i++)  {
@@ -38,31 +40,5 @@ function getPassword() {
   console.log(randChar)
   var password = document.getElementById("password")
   password.textContent = randChar
-    
-    
-  
 }
-
-
-// function getRandom() {
-
-// }
-
-// function generatePassword
-//   var result =[]
-//   result[Math.random ]
-// // get an array of all of the options
-
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-// Add event listener to generate button when this is clicked, it will run writePassword
 generateBtn.addEventListener("click", getPassword);
-
-
-// on click i want:
-  // 
