89b6cb71b1edf37a4c0b33ceb4574228da6e36c2
hmorton77 <harold.m.morton@gmail.com>
2020-09-13 10:53:25 -0400

    added step 3 and fixed up step 1

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 var lowerCaseCharacters = ["a",  "b",  "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numerals = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
 var specialCharacters = ['@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',  ',',  ')',  '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'];
 
+var numerals = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+// random number generation with min/max
+function random (min, max) {
+  return min + Math.random() * (max - min);
+}
+function getPassword() {
 // step 1: get characters req's
-function getPasswordOptions() {
 // conditions (number of letters, special symbols, etc.)
   var passLength = parseInt(prompt("How many characters are required for your password? (8-128 characters"));
-  if (passLength < 8)
-    var passLength = parseInt(prompt("Re-Enter Length: you must request a password greater than 8 characters!"));
-  else if (passLength > 128)
-    var passLength = parseInt(prompt("you must request a password with less than 128 characters!"));
+  while (passLength < 8 || passLength > 128) {
+    var passLength = parseInt(prompt("Re-Enter Length: you must request a password within the character limits! (8-128 characters)"))
+  }
   var special = confirm("Does your password need special characters?");
   var numbers = confirm("Does your password need numbers 0-9?");
 // step 2: put all the req'ed chars into 1 array. 
@@ -24,9 +29,18 @@ function getPasswordOptions() {
     var randArray = randArray.concat(specialCharacters)
   if (numbers)
     var randArray = randArray.concat(numerals)
-
-console.log(randArray)
-
+// step 3: from the array, randomly select x characters, x being the amount of characters req'ed  
+  randChar = ""
+  for (var i = 0; i < passLength; i++)  {
+    randNum = Math.floor(random(0,(randArray.length-1))) + 1;
+    console.log(randNum)
+    console.log(randArray[randNum])
+    randChar = randChar + randArray[randNum]
+  }
+  console.log(randChar)
+    
+    
+  
 }
 
 
@@ -47,7 +61,7 @@ console.log(randArray)
 
 // }
 // Add event listener to generate button when this is clicked, it will run writePassword
-generateBtn.addEventListener("click", getPasswordOptions);
+generateBtn.addEventListener("click", getPassword);
 
 
 // on click i want:
