fcf0280eb62a3a8c35770c313f131dead86b5ad3
hmorton77 <harold.m.morton@gmail.com>
2020-09-14 12:51:22 -0400

    added uppercase letters, added text to README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PasscodeGenerator
 Week 3 Homework
+**how it works**
+When the Generate Password is clicked, then prompts will ask the user various requirements for the password.
+Utilizing the user responses, the code will add characters into the array, and a random number generator will generate a number
+the number will be used to index the array, and pull the corresponding character out of the large array. 
+
+this is done consecutively in a loop, until the amount of characters are generated for the user's required length. 
+when complete, the password will be given in the read only text area. 
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 var lowerCaseCharacters = ["a",  "b",  "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseCharacters = ["A",  "B",  "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var specialCharacters = ['@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',  ',',  ')',  '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'];
 
@@ -16,7 +17,7 @@ function random (min, max) {
 function getPassword() {
 // step 1: get characters req's
 // conditions (number of characters, special symbols, etc.)
-  var passLength = parseInt(prompt("How many characters are required for your password? (8-128 characters"));
+  var passLength = parseInt(prompt("How many characters are required for your password? (8-128 characters)"));
   while (passLength < 8 || passLength > 128) {
     var passLength = parseInt(prompt("Re-Enter Length: you must request a password within the character limits! (8-128 characters)"))
   }
@@ -24,8 +25,8 @@ function getPassword() {
   var numbers = confirm("Does your password need numbers 0-9?");
   
 // step 2: put all the req'ed chars into 1 array. 
-  var randArray = []
-  var randArray = randArray.concat(lowerCaseCharacters)
+  var randArray = lowerCaseCharacters
+  var randArray = randArray.concat(upperCaseCharacters)
   if (special) 
     var randArray = randArray.concat(specialCharacters)
   if (numbers)
