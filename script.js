b393a726571d6e52c69acffacf21a475092ce43d
hmorton77 <harold.m.morton@gmail.com>
2020-09-15 12:29:11 -0400

    you can never have too many semicolons

--- script.js
+++ script.js
@@ -16,26 +16,27 @@ function getPassword() {
 // conditions (number of characters, special symbols, etc.)
   var passLength = parseInt(prompt("How many characters are required for your password? (8-128 characters)"));
   while (passLength < 8 || passLength > 128 || Number.isInteger(passLength) == false) {
-    var passLength = parseInt(prompt("Re-Enter Length: you must request an INTEGER within the character limits! (8-128 characters)"))
-  }
+    var passLength = parseInt(prompt("Re-Enter Length: you must request an INTEGER within the character limits! (8-128 characters)"));
+  };
   var upper = confirm("Does your password need both cases for letters? (Default: lowercase only)");
-  var special = confirm("Does your password need special characters?");
   var numbers = confirm("Does your password need numbers 0-9?");
+  var special = confirm("Does your password need special characters?");
+  
   
 // step 2: put all the req'ed chars into 1 array. 
   var randArray = lowerCaseCharacters
-  if (upper)
-    var randArray = randArray.concat(upperCaseCharacters)
-  if (special) 
-    var randArray = randArray.concat(specialCharacters)
-  if (numbers)
-    var randArray = randArray.concat(numerals)
+  if (upper);
+    randArray = randArray.concat(upperCaseCharacters);
+  if (special); 
+    randArray = randArray.concat(specialCharacters);
+  if (numbers);
+    randArray = randArray.concat(numerals);
 
 // step 3: from the array, randomly select x characters, x being the amount of characters req'ed  
   randChar = ""
   for (var i = 0; i < passLength; i++)  {
     randNum = Math.floor(random(0,(randArray.length-1))) + 1;
-    randChar = randChar + randArray[randNum]
+    randChar = randChar + randArray[randNum];
   }
   console.log(randChar)
   var password = document.getElementById("password")
